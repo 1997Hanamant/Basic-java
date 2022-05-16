7e152bd8008d2dd140818c4ef4f3a7e42b92535f
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-05-17 00:15:48 +0530

    my new

---
+++ New file/Bike.java
@@ -0,0 +1,17 @@
+class Bike
+{
+String name;
+String color;
+String speed;
+void printdetails(){
+System.out.println("name:"+name+"\n"+"color:"+color+"\n"+"speed:"+speed+"\n");
+public static void main(string[] args){
+Bike a=new Bike();
+a.name="tvs";
+a.color="green";
+a.speed="80kmph";
+a.printdetails();
+}
+}
+}
+
