433de8290d89addaaf9e64133d5ccc80e398777a
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-05-17 00:29:25 +0530

    increment

---
+++ New file/IncrementoperatorsDemo.java
@@ -0,0 +1,60 @@
+class IncrementoperatorsDemo
+{
+	public static void main(String args[])
+	    //pre increment
+	{
+		//int a=345;
+		//int b=++a;
+	 //int b=120;
+		
+		//System.out.println("b:"+a);
+		//System.out.println("b:"+b);
+		
+		//System.out.println("b:"+(a++));
+		//System.out.println("b:"+(b++));
+		
+		//System.out.println("b:"+((++a)+(++a)));
+		//System.out.println("b:"+(++b+b++));
+		
+		//int a=35;
+		//--a;
+		//int b=a;
+		
+		//System.out.println("b:"+(--b));
+		//System.out.println("b:"+((b++)+(--a)));
+		//System.out.println("b:"+((--a)+(a++)));
+		//System.out.println("b:"+((++b)+(a--)));
+		//System.out.println(a);
+		//System.out.println(b);
+		
+	//int a=10;
+	//int b=20;
+	//int c=30;
+	//c=a;
+	//--a;
+	//System.out.println(c++);
+	//System.out.println(a++);
+	//System.out.println(b--);
+	//System.out.println((--b)+(--c));
+	//System.out.println(a);
+	//System.out.println(b);
+	//System.out.println(c);
+	
+	int x=90;
+	int y=67;
+	int z=78;
+	 x=y;
+	 y=z;
+	System.out.println(x++);
+	System.out.println((--z)+(--y));
+	System.out.println((y++)+(++z));
+	System.out.println((x++)+(z++));
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	
+	
+	
+	}
+}
+	
