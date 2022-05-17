d8f00fe4aacfaae34d192368c0ece7242e41bbc5
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-05-17 15:43:00 +0530

    odd and even program

---
+++ New file/even.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class even
+{
+public static void main(String args[])
+{
+Scanner Scn=Scanner(System.in);
+System.out.println("enter a even number");
+int num=Scn.nextInt();
+if(num %2==0)
+System.out.println(num+"is even");
+else
+System.out.println(num+"is odd");
+}
+}
