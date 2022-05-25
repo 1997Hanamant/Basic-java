e4487c61a3a3e4ece1733a9ed019edd0f43e9ce2
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-05-26 00:57:50 +0530

    Method programms

---
+++ New file/Method/methodDemo.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+class methodDemo
+{
+//static int mul(int a,int b){
+	//int mul=a*b;
+
+//return mul;
+//return a*b;
+//}
+
+//public static void main(String args[])
+//{
+	//int result;
+	//result=mul(2,3);
+//System.out.println(result);
+//}
+
+//int add(int a,int b){
+	//int add=a+b;
+	//return add;
+//}
+//public static void main(String args[]){
+	//int a=10;
+	//int b=20;
+	//methodDemo s=new methodDemo();
+	//int result=s.add(a,b);
+	//System.out.println(result);
+//}
+//int diff(int x,int y){
+	//int diff=x-y;
+	//return diff;
+//}
+//public static void main(String args[]){
+	//int x=100;
+	//int y=20;
+	//methodDemo b=new methodDemo();
+	//int result=b.diff(x,y);
+	//System.out.println(result);
+//}
+	
+//static int diff(int x,int y){
+	//int diff=x-y;
+	//return diff;
+//}
+//public static void main(String args[]){
+	//System.out.println(diff(100,50));
+	//System.out.println(diff(200,100));
+//}
+ //void square(int num){
+	//int square=2*2;
+//}
+//public static void main(String args[]){
+	//System.out.println("square is 4 ");
+//}
+//method parameter
+//void display1(){
+	//System.out.println("method without parameter");
+//}
+//void display2(int a){
+	//System.out.println("method with parameter"+a);
+//}
+//public static void main(String args[]){
+	//methodDemo r=new methodDemo();
+	//r.display1();
+	//r.display2(12);
+//}
+
+ public int Evenodd(){
+ 
+ int Evenodd;
+ }
+	public static void main(String args[]){
+		Scanner Sacn=new Scanner(System.in);
+		System.out.println("enter a number");
+		int num=Scan.nextInt();
+		Evenodd(3);
+	}
+}
