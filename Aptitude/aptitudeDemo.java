76e6a2ca5451882ccfb80b384a24c8f17698d490
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-06-05 00:33:58 +0530

    Aptitude

---
+++ Aptitude/aptitudeDemo.java
@@ -0,0 +1,38 @@
+class aptitudeDemo
+{
+public static void main(String args[])
+{
+	//double x=10.5;
+	// x/=4+2.5*2.5;
+	 //System.out.println(x);
+	 
+	 //int a,b,exp=10;
+	 //a=b=5;
+	 //exp+=++a*b++;
+	 //System.out.println(+exp);
+	 
+	 //int a,b,exp=10;
+	 //a=b=5;
+	 //exp*=++a*10/b++ + --a;
+	 //System.out.println(+exp);
+	  
+	  //int x=5;
+	  //int y=2,exp=10;
+	  
+	  //exp%=x+(x<6?++y:--y);
+	  //System.out.println(+exp);
+	  
+	  int a=4;
+	  int x=a<5?1+2:1*1;
+	  System.out.println(x);
+	  
+	  //if(++x<4)
+		//  x+=1;
+	 // System.out.println(x);
+	  
+	  //int x=5,y;
+	 // y=++x + x++ + --x;
+	  //System.out.println(x+" "+y);
+	  
+}
+}
