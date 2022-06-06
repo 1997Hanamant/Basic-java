0c4335a73e220de0520f97e7b34b8012d922ca6a
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-06-06 12:23:39 +0530

    Aptitude pattern1Demo patternDemo

---
+++ New file/child.java
@@ -0,0 +1,12 @@
+class child{
+String name;
+String id;
+String dob;
+int age;
+void printdetails(){
+System.out.println("id:"+id+"\n"+
+                   "name:"+name+"\n"+
+				   "dob:"+dob+"\n"+
+				   "age:"+age+"\n");
+				   }
+				   }
